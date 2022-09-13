db7b5a525e0ea1c85be658c89117c657ebb5213e
Marina Paley <marina_paley@mail.ru>
2022-09-13 13:01:53 +0300

    Начало работы с Заданием 1.1 #1

---
+++ Task 1/Task 1.1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+/**
+* \brief Расчет а по заданной формуле.
+* \param x Первый аргумент функции.
+* \param y Второй аргумент функции.
+* \return Значение a.
+*/
+double GetA(const double x, const double y);
+
+/**
+* \brief Расчет а по заданной формуле.
+* \param z Первый аргумент функции.
+* \return Значение a.
+*/
+double GetB(const double z);
+
+/**
+ * \brief Точка входа в программу
+ * \return 0 в случае успеха
+ */
+int main()
+{
+    const auto x = 1.426;
+    const auto y = -1.22;
+    const auto z = 3.5;
+    const auto a = GetA(x, y);
+    const auto b = GetB(z);
+    std::cout << "a = " << a <<", b = " << b <<" \nx = " << x 
+        << ", y = " << y << ", z = " << z;
+    std::cout << std::endl;
+    return 0; // Это однострочный комментарий. Код ошибки
+}
